dde07531256338bbc58b0671ce215ec6d9e69c09
raaang <graytorgb@gmail.com>
2022-08-12 15:46:52 +0900

    Feat: 폴더 구조 생성

---
+++ src/redux/modules/comment.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+
+}
+
+const comment = createSlice({
+	name: "comment",
+	initialState,
+	reducers: {
+		
+	}
+});
+
+export const {} = comment.actions;
+export default comment.reducer;
