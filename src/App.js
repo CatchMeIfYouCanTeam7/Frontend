7e8db9281c603cd31aa6c56168d0b4c9857bb3e6
raaang <graytorgb@gmail.com>
2022-08-12 16:23:52 +0900

    Feat: router 기본 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
+import { Main, Posting, Detail, Login, SignUp } from "./pages";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Posting" element={<Posting />} />
+        <Route path="/Detail" element={<Detail />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
