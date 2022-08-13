aeead2500c78e7efd30007855b848b09f846cd10
4775614 <4775614@naver.com>
2022-08-13 12:00:57 +0900

    App.js수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { Route, Routes } from "react-router-dom";
-import { Main, Posting, Detail, Login, SignUp } from "./pages";
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Posting from './pages/Posting';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/Posting" element={<Posting />} />
-        <Route path="/Detail" element={<Detail />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/Posting" element={<Posting />} />
+				<Route path="/Detail" element={<Detail />} />
+				<Route path="/Login" element={<Login />} />
+				<Route path="/SignUp" element={<SignUp />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
