134a298b744f8d4abd4c8b08da282eff2b3b1e4e
4775614 <4775614@naver.com>
2022-08-18 19:28:38 +0900

    modify:sign up CSS 수정

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,64 +1,70 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Button from "../button/Button";
-import axios from "axios";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../button/Button';
+import axios from 'axios';
 
 // Style import
-import { CatchLogo, StHeader } from "./Header.styled";
+import { CatchLogo, StHeader } from './Header.styled';
 
 const Header = ({ userId }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
+	const LogoutHandler = () => {
+		// console.log(JSON.parse(localStorage.getItem("accessToken" + userId)));
+		axios
+			.get(process.env.REACT_APP_URL + '/api/auth/members/logout', {
+				headers: {
+					Authorization: JSON.parse(
+						localStorage.getItem('accessToken' + userId)
+					).auth,
+				},
+			})
+			.then((res) => {})
+			.catch((error) => {});
 
-  const LogoutHandler = () => {
-    // console.log(JSON.parse(localStorage.getItem("accessToken" + userId)));
-    axios
-      .get(process.env.REACT_APP_URL + "/api/auth/members/logout", {
-        headers: {
-          Authorization: JSON.parse(
-            localStorage.getItem("accessToken" + userId)
-          ).auth,
-        },
-      })
-      .then((res) => {
-      })
-      .catch((error) => {
-      });
+		localStorage.removeItem('accessToken' + userId);
 
-    localStorage.removeItem("accessToken" + userId);
+		alert('로그아웃 하였습니다!');
+		navigate('/');
+	};
 
-    alert("로그아웃 하였습니다!");
-    navigate('/')
-  };
-
-  return (
-    <StHeader>
-      <CatchLogo onClick={() => navigate("/")} />
-      <div style={{ marginLeft: "auto" }}></div>
-      &nbsp;&nbsp;
-      {localStorage.length>0 ?
-      <Button
-        onClick={LogoutHandler}
-        className="signUpBox"
-        style={{ marginRight: "20px", width: "100px" }}
-      >
-        로그아웃
-      </Button> :
-      <><Button
-          onClick={() => navigate("/Login")}
-          className="signUpBox"
-          style={{ marginRight: "20px", width: "100px" }}
-        >
-          로그인
-        </Button><Button
-          onClick={() => navigate("/SignUp")}
-          className="signUpBox"
-          style={{ marginRight: "20px", width: "100px" }}
-        >
-            회원가입
-          </Button></>};
-    </StHeader>
-  );
+	return (
+		<StHeader>
+			<CatchLogo onClick={() => navigate('/')} />
+			<div style={{ marginLeft: 'auto' }}></div>
+			&nbsp;&nbsp;
+			{localStorage.length > 0 ? (
+				<Button
+					id="SignUpBtn"
+					onClick={LogoutHandler}
+					className="signUpBox"
+					style={{ marginRight: '20px', width: '100px' }}
+				>
+					LOG OUT
+				</Button>
+			) : (
+				<>
+					<Button
+						id="logInBtn"
+						onClick={() => navigate('/Login')}
+						className="signUpBox"
+						style={{ marginRight: '20px', width: '100px' }}
+					>
+						LOG IN
+					</Button>
+					<Button
+						id="signUpBtn"
+						onClick={() => navigate('/SignUp')}
+						className="signUpBox"
+						style={{ marginRight: '20px', width: '100px' }}
+					>
+						SIGN UP
+					</Button>
+				</>
+			)}
+			;
+		</StHeader>
+	);
 };
 
 export default Header;
--- src/pages/signup/SignUp.styled.js
+++ src/pages/signup/SignUp.styled.js
@@ -1,14 +1,31 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StSignUpDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 350px;
-  align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	/* justify-content: space-between; */
+	/* height: 400px; */
+	align-items: flex-start;
+
+  .signUpItem {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    height: 100px;
+  }
+
+	label {
+		margin-top: 10px;
+		margin-left: 2px;
+	}
+  span {
+    margin-top: 5px;
+    font-size: 15px;
+    margin-left: 2px;
+  }
 `;
 
 export const StSignUpForm = styled.form`
-  height: 550px;
-  text-align: center;
+	height: 500px;
+	text-align: center;
 `;
